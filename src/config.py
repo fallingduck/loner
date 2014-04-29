627cf2d5fdcf844e624475946ec29861d8d7cc2b
fallingduck <jack@fallingduck.net>
2014-04-28 20:30:18 -0700

    Added a setup procedure, switched config to use YAML

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# TODO: Load from file
-config = {'server': {'address': '[::1]:53',
-                     'request_timeout': 2},
-          'nameservers': ['2620:0:ccc::2', '2620:0:ccd::2']}
+import yaml
+
+
+with open('/etc/loner.conf', 'r') as f:
+    config = yaml.load(f)
